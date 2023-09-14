79bb90b4c2890e2b479f76f4693a6cb404e7e36f
aboba100 <112294807+Aboba1001@users.noreply.github.com>
2023-09-15 00:06:10 +0400

    Update GameplaySettingsSubState.hx

--- source/options/GameplaySettingsSubState.hx
+++ source/options/GameplaySettingsSubState.hx
@@ -52,7 +52,7 @@ class GameplaySettingsSubState extends BaseOptionsMenu
 			false); //Default value
 		addOption(option);
 
-		var option:Option = new Option('Display MS Offset On Note Hits',
+		var option:Option = new Option('Show MS Text',
 			'If checked, a offset (in ms) will appear near notes',
 			'showMsText',
 			'bool',
