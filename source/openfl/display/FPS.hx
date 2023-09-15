24f2a93dbe99b8a511e27e9a4e88501a5acbda4d
aboba100 <112294807+Aboba1001@users.noreply.github.com>
2023-09-16 00:15:19 +0400

    Update FPS.hx

--- source/openfl/display/FPS.hx
+++ source/openfl/display/FPS.hx
@@ -90,7 +90,7 @@ class FPS extends TextField
 			memoryMegas = Math.abs(FlxMath.roundDecimal(System.totalMemory / 1000000, 1));
 			text += "\nMEM: " + memoryMegas + " MB";
 			text += "\nMEM Peak: " + memoryMegas + " MB";
-			text += "\nEngine Version: " + MainMenuState.engineVersion;
+			text += "\nEngine Version: " + MainMenuState.abobaEngineVersion;
 			text += "\nPsych Engine Version: " + MainMenuState.psychEngineVersion;
 			#end
 
