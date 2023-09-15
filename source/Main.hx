1c3a80e541a9f05bf664ba7d6fb888ea59483f62
aboba100 <112294807+Aboba1001@users.noreply.github.com>
2023-09-15 15:44:31 +0400

    Update Main.hx

--- source/Main.hx
+++ source/Main.hx
@@ -93,7 +93,7 @@ class Main extends Sprite
 		ClientPrefs.loadDefaultKeys();
 		addChild(new FlxGame(game.width, game.height, game.initialState, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
-		fpsVar = new FPS(8, 3, 0xFFFFFF);
+		fpsVar = new FPS(10, 3, 0xFFFFFF);
 		addChild(fpsVar);
 		Lib.current.stage.align = "tl";
 		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
