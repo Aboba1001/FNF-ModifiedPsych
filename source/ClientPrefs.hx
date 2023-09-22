f9b8d531ecabc450a1f6a98d215df56d5c6048fc
AbobaGitHub <112294807+Aboba1001@users.noreply.github.com>
2023-09-22 11:44:12 +0400

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -68,7 +68,7 @@ class ClientPrefs {
 
 	public static var comboOffset:Array<Int> = [0, 0, 0, 0];
 	public static var ratingOffset:Int = 0;
-	public static var perfectWindoe:Int = 15;
+	public static var perfectWindow:Int = 15;
 	public static var sickWindow:Int = 45;
 	public static var goodWindow:Int = 90;
 	public static var badWindow:Int = 135;
