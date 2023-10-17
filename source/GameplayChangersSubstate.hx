a733beb0439da397ca0dc68cd5453f231997cc3d
AbobaGitHub <112294807+Aboba1001@users.noreply.github.com>
2023-10-17 18:18:06 +0400

    Update GameplayChangersSubstate.hx

--- source/GameplayChangersSubstate.hx
+++ source/GameplayChangersSubstate.hx
@@ -95,7 +95,7 @@ class GameplayChangersSubstate extends MusicBeatSubstate
 
 		var option:GameplayOption = new GameplayOption('Botplay', 'botplay', 'bool', false);
 		optionsArray.push(option);
-
+	}
 	public function getOptionByName(name:String)
 	{
 		for(i in optionsArray)
