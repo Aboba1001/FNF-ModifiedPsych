a3b48529d87821cfcf7d8b646f27873552df5654
AbobaGitHub <112294807+Aboba1001@users.noreply.github.com>
2023-10-25 16:31:00 +0400

    Update Controls.hx

--- source/Controls.hx
+++ source/Controls.hx
@@ -21,29 +21,7 @@ import android.flixel.FlxButton as FlxNewButton;
 #end
 	
 #if (haxe >= "4.0.0")
-{
-var UI_DOWN_R = "ui_down-release";
-	var NOTE_UP = "note_up";
-	var NOTE_LEFT = "note_left";
-	var NOTE_RIGHT = "note_right";
-	var NOTE_DOWN = "note_down";
-	var NOTE_UP_P = "note_up-press";
-	var NOTE_LEFT_P = "note_left-press";
-	var NOTE_RIGHT_P = "note_right-press";
-	var NOTE_DOWN_P = "note_down-press";
-	var NOTE_UP_R = "note_up-release";
-	var NOTE_LEFT_R = "note_left-release";
-	var NOTE_RIGHT_R = "note_right-release";
-	var NOTE_DOWN_R = "note_down-release";
-	var ACCEPT = "accept";
-	var BACK = "back";
-	var PAUSE = "pause";
-	var RESET = "reset";
-}
-#else
-@:enum
-abstract Action(String) to String from String
-{
+enum abstract Action(String) to String from String {
 	var UI_UP = "ui_up";
 	var UI_LEFT = "ui_left";
 	var UI_RIGHT = "ui_right";
@@ -72,35 +50,44 @@ abstract Action(String) to String from String
 	var BACK = "back";
 	var PAUSE = "pause";
 	var RESET = "reset";
+	var SPACE = 'space';
+	var SPACE_P = 'space-press';
+	var SPACE_R = 'space-release';
+}
+#else
+@:enum
+abstract Action(String) to String from String {
+	var UI_UP = "ui_up";
+	var UI_LEFT = "ui_left";
+	var UI_RIGHT = "ui_right";
+	var UI_DOWN = "ui_down";
+	var UI_UP_P = "ui_up-press";
+	var UI_LEFT_P = "ui_left-press";
+	var UI_RIGHT_P = "ui_right-press";
+	var UI_DOWN_P = "ui_down-press";
+	var UI_UP_R = "ui_up-release";
+	var UI_LEFT_R = "ui_left-release";
+	var UI_RIGHT_R = "ui_right-release";
+	var UI_DOWN_R = "ui_down-release";
+	var NOTE_UP = "note_up";
+	var NOTE_LEFT = "note_left";
+	var NOTE_RIGHT = "note_right";
+	var NOTE_DOWN = "note_down";
+	var NOTE_UP_P = "note_up-press";
+	var NOTE_LEFT_P = "note_left-press";
+	var NOTE_RIGHT_P = "note_right-press";
+	var NOTE_DOWN_P = "note_down-press";
+	var NOTE_UP_R = "note_up-release";
+	var NOTE_LEFT_R = "note_left-release";
+	var NOTE_RIGHT_R = "note_right-release";
+	var NOTE_DOWN_R = "note_down-release";
+	var ACCEPT = "accept";
+	var BACK = "back";
+	var PAUSE = "pause";
+	var RESET = "reset";
+	var SPACE = 'space';
 }
 #end
-
-enum Device
-{
-	Keys;
-	Gamepad(id:Int);
-}
-
-/**
- * Since, in many cases multiple actions should use similar keys, we don't want the
- * rebinding UI to list every action. ActionBinders are what the user percieves as
- * an input so, for instance, they can't set jump-press and jump-release to different keys.
- */
-enum Control
-{
-	UI_UP;
-	UI_LEFT;
-	UI_RIGHT;
-	UI_DOWN;
-	NOTE_UP;
-	NOTE_LEFT;
-	NOTE_RIGHT;
-	NOTE_DOWN;
-	RESET;
-	ACCEPT;
-	BACK;
-	PAUSE;
-}
 
 enum KeyboardScheme
 {
