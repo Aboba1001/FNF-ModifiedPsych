c43fb41a59c6991893e723f956bc45cca7c6efa0
AbobaGitHub <112294807+Aboba1001@users.noreply.github.com>
2023-11-02 15:42:43 +0400

    Update PlayState.hx

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -1048,7 +1048,7 @@ class PlayState extends MusicBeatState
 			timeTxt.text = SONG.song;
 		}
 		updateTime = showTime;
-
+			
 		timeBarBG = new AttachedSprite('long_TimeBar');
 		timeBarBG.x = timeTxt.x;
 	        timeBarBG.y = timeTxt.y + (timeTxt.height / 4);
@@ -1209,7 +1209,7 @@ class PlayState extends MusicBeatState
 		add(iconP2);
 		reloadHealthBarColors();
 
-	        var songTxt = new FlxText(0, FlxG.height - 18, 0, SONG.song + " - " + CoolUtil.difficultyString() + " | XL Engine " + MainMenuState.abobaEngineVersion, 16);
+	        var songTxt = new FlxText(0, FlxG.height - 18, 0, SONG.song + " - " + CoolUtil.difficultyString() + " | RT Engine " + MainMenuState.abobaEngineVersion, 16);
 	        songTxt.setFormat(Paths.font("vcr.ttf"), 15, FlxColor.WHITE, RIGHT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
 		songTxt.scrollFactor.set();
 		songTxt.visible = !ClientPrefs.watermark && !ClientPrefs.hideHud;
