7dcac0449af1e996cbdc8c4f1f4a5c8cf47c4f28
AbobaGitHub <112294807+Aboba1001@users.noreply.github.com>
2023-11-16 15:52:12 +0400

    Update FunkinLua.hx

--- source/FunkinLua.hx
+++ source/FunkinLua.hx
@@ -163,7 +163,7 @@ class FunkinLua {
 		set('rating', 0);
 		set('ratingName', '');
 		set('ratingFC', '');
-		set('version', MainMenuState.psychEngineVersion.trim());
+		set('version', MainMenuState.abobaEngineVersion.trim());
 
 		set('inGameOver', false);
 		set('mustHitSection', false);
