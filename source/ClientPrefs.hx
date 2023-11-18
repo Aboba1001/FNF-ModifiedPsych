02edc91ac643eb50bf90fda199cd58ca0996c2b7
AbobaGitHub <112294807+Aboba1001@users.noreply.github.com>
2023-11-18 15:20:48 +0400

    Update ClientPrefs.hx

--- source/ClientPrefs.hx
+++ source/ClientPrefs.hx
@@ -7,7 +7,7 @@ import flixel.graphics.FlxGraphic;
 import Controls;
 
 class ClientPrefs {
-	public static var smJudges:Booo = false;
+	public static var smJudges:Bool = false;
 	public static var downScroll:Bool = false;
 	public static var middleScroll:Bool = false;
 	public static var opponentStrums:Bool = true;
