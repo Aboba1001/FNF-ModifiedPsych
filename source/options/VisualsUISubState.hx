605cda3f3a5a1d9f3b30e9e4c76d261c801ce72b
AbobaGitHub <112294807+Aboba1001@users.noreply.github.com>
2023-12-24 12:55:51 +0400

    Update VisualsUISubState.hx

--- source/options/VisualsUISubState.hx
+++ source/options/VisualsUISubState.hx
@@ -129,7 +129,7 @@ class VisualsUISubState extends BaseOptionsMenu
 			'timeBarType',
 			'string',
 			'Time Left',
-			['Time Left', 'Time Elapsed', 'Song Name', 'Disabled']);
+			['Time Left', 'Elapsed / Length', 'Time Elapsed', 'Song Name', 'Disabled']);
 		addOption(option);
 
 		var option:Option = new Option('Show RT Engine Watermark',
